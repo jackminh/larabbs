6d15218c6f47f191b8aad38a98381674bb36e10f
jackminh <jackminh@outlook.com>
2023-02-09 13:27:59 +0000

    the password is successfully retrieved.

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,26 +5,32 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
 
-class ResetPasswordController extends Controller
-{
-    /*
-    |--------------------------------------------------------------------------
-    | Password Reset Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller is responsible for handling password reset requests
-    | and uses a simple trait to include this behavior. You're free to
-    | explore this trait and override any methods you wish to tweak.
-    |
-    */
+class ResetPasswordController extends Controller {
+	/*
+		    |--------------------------------------------------------------------------
+		    | Password Reset Controller
+		    |--------------------------------------------------------------------------
+		    |
+		    | This controller is responsible for handling password reset requests
+		    | and uses a simple trait to include this behavior. You're free to
+		    | explore this trait and override any methods you wish to tweak.
+		    |
+	*/
 
-    use ResetsPasswords;
+	use ResetsPasswords;
 
-    /**
-     * Where to redirect users after resetting their password.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
+	/**
+	 * Where to redirect users after resetting their password.
+	 *
+	 * @var string
+	 */
+	protected $redirectTo = RouteServiceProvider::HOME;
+
+	protected function sendResetResponse(Request $request, $response) {
+		session()->flash('success', '密码更新成功，您已成功登录');
+		return redirect($this->redirectPath());
+	}
+
 }
