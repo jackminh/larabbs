8e8a8fc103431aa2b7f732a27f218bc0d87afc10
jackminh <jackminh@outlook.com>
2023-02-10 05:36:34 +0000

    upload avatars

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Handlers\ImageUploadHandler;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
 
@@ -15,8 +16,15 @@ class UsersController extends Controller {
 		return view('users.edit', compact('user'));
 	}
 
-	public function update(UserRequest $request, User $user) {
-		$user->update($request->all());
+	public function update(UserRequest $request, User $user, ImageUploadHandler $uploader) {
+		$data = $request->all();
+		if ($request->avatar) {
+			$result = $uploader->save($request->avatar, 'avatars', $user->id);
+			if ($request) {
+				$data['avatar'] = $result['path'];
+			}
+		}
+		$user->update($data);
 		return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
 	}
 
