293d635db5a9c36aa04a5c33c3e78d78398c3334
jackminh <jackminh@outlook.com>
2023-02-11 06:22:11 +0000

    create topic

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
 use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TopicsController extends Controller {
 	public function __construct() {
@@ -27,11 +29,14 @@ class TopicsController extends Controller {
 	}
 
 	public function create(Topic $topic) {
-		return view('topics.create_and_edit', compact('topic'));
+		$categories = Category::all();
+		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
-	public function store(TopicRequest $request) {
-		$topic = Topic::create($request->all());
+	public function store(TopicRequest $request, Topic $topic) {
+		$topic->fill($request->all());
+		$topic->user_id = Auth::id();
+		$topic->save();
 		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
 	}
 
