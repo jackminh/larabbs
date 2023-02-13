be003b1a295bafb757fd0a53295d9b7d7bffe9af
jackminh <jackminh@outlook.com>
2023-02-13 06:49:23 +0000

    user default avatra

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -7,15 +7,17 @@ use App\Models\User;
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
-class UserObserver
-{
-    public function creating(User $user)
-    {
-        //
-    }
+class UserObserver {
+	public function creating(User $user) {
+		//
+	}
 
-    public function updating(User $user)
-    {
-        //
-    }
+	public function updating(User $user) {
+		//
+	}
+	public function saving(User $user) {
+		if (empty($user->avatar)) {
+			$user->avatar = 'https://cdn.learnku.com/uploads/images/201710/30/1/TrJS40Ey5k.png';
+		}
+	}
 }
